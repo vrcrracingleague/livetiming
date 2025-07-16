8a769b57c7262f9eb2feff67b454ee278c10a8c5
vRCr Racing League <vrcrracingleague@gmail.com>
2025-07-16 23:08:50 +0200

    Add files via upload

---
+++ screenshot.js
@@ -0,0 +1,25 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: "new",
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  });
+
+  const page = await browser.newPage();
+  await page.goto('https://vrcrracingleague.github.io/', {
+    waitUntil: 'networkidle2',
+    timeout: 0,
+  });
+
+  await page.setViewport({ width: 1280, height: 720 });
+
+  await page.screenshot({
+    path: 'livetiming.jpg',
+    type: 'jpeg',
+    quality: 80,
+    fullPage: true
+  });
+
+  await browser.close();
+})();
