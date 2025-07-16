044fa310cbfa6ae07e2196e6c9139d6e657a8e38
vRCr Racing League <vrcrracingleague@gmail.com>
2025-07-17 00:04:10 +0200

    Update screenshot.js

--- screenshot.js
+++ screenshot.js
@@ -7,12 +7,16 @@ const puppeteer = require('puppeteer');
   });
 
   const page = await browser.newPage();
-await page.goto('https://vrcrracingleague.github.io/livetiming/', {
-  waitUntil: 'networkidle2',
-  timeout: 0,
-});
+
+  await page.goto('https://vrcrracingleague.github.io/livetiming/', {
+    waitUntil: 'networkidle2',
+    timeout: 0,
+  });
 
   await page.setViewport({ width: 1280, height: 720 });
+
+  // Aspetta 5 secondi per far caricare i dati live
+  await page.waitForTimeout(5000);
 
   await page.screenshot({
     path: 'livetiming.jpg',
